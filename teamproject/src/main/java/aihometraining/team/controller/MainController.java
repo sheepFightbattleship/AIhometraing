ac914d2a3469aa04cea0cd08009161ca1332c4bf
qpfn1323 <qpfn1323@naver.com>
2022-03-15 10:47:33 +0900

    [team1] 기본 레이아웃 구성 detail: aplication.properties 파일을 제외한 프로젝트 리소스 업로드

---
+++ teamproject/src/main/java/aihometraining/team/controller/MainController.java
@@ -0,0 +1,25 @@
+package aihometraining.team.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String main(Model model) {
+		
+		model.addAttribute("title", "메인화면");
+		
+		return "main";
+	}
+	@GetMapping("/user")
+	public String usermain(Model model) {
+		
+		model.addAttribute("title", "메인화면");
+		
+		return "usermain";
+		
+	}
+}
